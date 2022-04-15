4f135ed64dbf3d243f24e662ba5ccd1fbcde6cc3
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-15 12:00:53 -0500

    cambios y url

--- backend/src/main/java/com/jdvpl/jdvpl/repository/ItemCategoryRepository.java
+++ backend/src/main/java/com/jdvpl/jdvpl/repository/ItemCategoryRepository.java
@@ -2,6 +2,5 @@ package repository;
 
 import model.ItemCategoryModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 public interface ItemCategoryRepository extends MongoRepository<ItemCategoryModel, String> {
 }
